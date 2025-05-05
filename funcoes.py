93908b0a8c4474e91a018daf6636cf87236add94
Enrique <enriquevp@al.insper.edu.br>
2025-05-05 13:53:17 -0300

    Exercício 3

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,10 @@ def guardar_dado(dados_rolados: list[int], dados_no_estoque: list[int], indice: 
     valor = novos_rolados.pop(indice)
     novos_estoque.append(valor)
     return [novos_rolados, novos_estoque]
+
+def remover_dado(dados_rolados: list[int], dados_no_estoque: list[int], indice: int) -> list[list[int]]:
+    novos_rolados = dados_rolados.copy()
+    novos_estoque = dados_no_estoque.copy()
+    valor = novos_estoque.pop(indice)
+    novos_rolados.append(valor)
+    return [novos_rolados, novos_estoque]
