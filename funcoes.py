f3eb9b8087cf777b5707810a9427f13ed16f45b7
Enrique <enriquevp@al.insper.edu.br>
2025-05-05 13:45:25 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -3,3 +3,9 @@ import random
 def rolar_dados(qtd_dados: int) -> list[int]:
     return [random.randint(1, 6) for _ in range(qtd_dados)]
 
+def guardar_dado(dados_rolados: list[int], dados_no_estoque: list[int], indice: int) -> list[list[int]]:
+    novos_rolados = dados_rolados.copy()
+    novos_estoque = dados_no_estoque.copy()
+    valor = novos_rolados.pop(indice)
+    novos_estoque.append(valor)
+    return [novos_rolados, novos_estoque]
